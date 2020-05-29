d78688e03f651895e3015e5148ca4ffac8a4417c
Michael Byrd <mcbyrd100@gmail.com>
2020-05-29 00:41:58 -0500

    Bug fix for the features used and comments for each function.

--- src/utility.R
+++ src/utility.R
@@ -1,3 +1,6 @@
+## Create arms for the simulation study.
+# n_arms: number of arms for the experiment
+# n_features: number of features for each arm.
 createArms <- function(n_arms, n_features) {
     return(
         list(
@@ -12,18 +15,23 @@ createArms <- function(n_arms, n_features) {
     )
 }
 
+## Create a new instance to be given a recommendation.
+# n_features: number of features for the instance.
 createInstance <- function(n_features) {
     return(rnorm(n = n_features))
 }
 
+## Create the true coefficients for a simulation study.
+# n_arm_features: Number of arm features.
+# n_instance_features: Number of instance features.
 createCoefficients <- function(
-    n_arms, 
     n_arm_features, 
     n_instance_features
 ) {
     # the total number of features
+    # don't directly use instance as it doesn't distinguish info
     # includes interactions between instance and arm
-    p = n_arm_features + n_instance_features + n_arm_features*n_instance_features
+    p = n_arm_features + n_arm_features*n_instance_features
 
     return(
         list(
@@ -37,6 +45,24 @@ createCoefficients <- function(
     )
 }
 
+## For two feature matrices of same number of rows, create feature interactions.
+# m1: Matrix#1 
+# m2: Matrix#2 of the same number of rows as m1
+makeInteractions <- function(m1, m2) {
+    return (
+        do.call(
+            what = cbind,
+            args = lapply(1:ncol(m1), function(i) {
+                m1[,i] * m2
+            })
+        )
+    )
+}
+
+## Calculate the probability of success for each arm
+# instance: instance features
+# arms: arm tracker list
+# coefficients: p \times 1 matrix of model coefficients
 calcChoiceProbability <- function(
     instance, 
     arms, 
@@ -54,11 +80,9 @@ calcChoiceProbability <- function(
         # arm features
         arms$features,
         # interaction of instance and arm features - hacky but idk how else
-        do.call(
-            what = cbind,
-            args = lapply(1:length(instance), function(i) {
-                broadcast_instance[,i] * arms$features
-            })
+        makeInteractions(
+            m1 = broadcast_instance,
+            m2 = arms$features
         )
     )
 
