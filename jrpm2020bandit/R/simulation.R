abbc1f65305ad21f7834357606a7a4053e967c1c
Michael Byrd <mcbyrd100@gmail.com>
2020-06-03 00:15:26 -0500

    Modified simulation wrapper to handle multiple settings for pg-ts in the same experiment.

--- jrpm2020bandit/R/simulation.R
+++ jrpm2020bandit/R/simulation.R
@@ -11,6 +11,7 @@ banditSimulation <- function(
     if (length(n_sample) != length(window_size)) {
         stop('n_sample and window_size must be the same length')
     }
+    n_pgts_settings <- length(n_sample)
 
     # arms and underlying linear relationship
     arms <- createArms(
@@ -25,7 +26,10 @@ banditSimulation <- function(
     # performance evaluated by regret at each time point for each method
     random_regret <- rep(NA, horizon)
     mab_regret <- rep(NA, horizon)
-    pgts_regret <- rep(NA, horizon)
+    pgts_regret <- matrix(
+        data = rep(NA, horizon*n_pgts_settings),
+        ncol = n_pgts_settings
+    )
 
     # tracker for mab and pgts
     mab_tracker <- mabCreateContainer(
@@ -33,15 +37,20 @@ banditSimulation <- function(
         alpha = alpha,
         beta = beta
     )
-    pgts_tracker <- pgtsCreateContainer(
-        mu = matrix(
-            data = rep(0, true_coef$p),
-            nrow = true_coef$p,
-            ncol = 1
-        ),
-        sigma = diag(1, true_coef$p),
-        n_sample = n_sample,
-        window_size = window_size
+    pgts_trackers <- lapply(
+        X = 1:n_pgts_settings,
+        FUN = function(j) {
+                pgtsCreateContainer(
+                mu = matrix(
+                    data = rep(0, true_coef$p),
+                    nrow = true_coef$p,
+                    ncol = 1
+                ),
+                sigma = diag(1, true_coef$p),
+                n_sample = n_sample[j],
+                window_size = window_size[j]
+            )
+        }
     )
 
     for (i in 1:horizon) { 
@@ -53,10 +62,15 @@ banditSimulation <- function(
             size = 1
         )
         mab_suggest <- mabRecommend(mab_tracker)
-        pgts_suggest <- pgtsRecommend(
-            pgts_container = pgts_tracker,
-            instance = instance,
-            arms = arms
+        pgts_suggests <- lapply(
+            X = pgts_trackers,
+            FUN = function(tracker) {
+                pgtsRecommend(
+                    pgts_container = tracker,
+                    instance = instance,
+                    arms = arms
+                )
+            }
         )
 
         # calculate truth, identify success, and calculate regret
@@ -76,16 +90,26 @@ banditSimulation <- function(
             size = 1,
             prob = succ_prob[mab_suggest]
         )
-        pgts_success <- rbinom(
-            n = 1,
-            size = 1,
-            prob = succ_prob[pgts_suggest]
+        pgts_successes <- lapply(
+            X = pgts_suggests,
+            FUN = function(suggest) {
+                rbinom(
+                    n = 1,
+                    size = 1,
+                    prob = succ_prob[suggest]
+                )
+            }
         )
 
         ideal_prob <- max(succ_prob)
         random_regret[i] <- ideal_prob - succ_prob[random_suggest]
         mab_regret[i] <- ideal_prob - succ_prob[mab_suggest]
-        pgts_regret[i] <- ideal_prob - succ_prob[pgts_suggest]
+        pgts_regret[i,] <- sapply(
+            X = pgts_suggests,
+            FUN = function(suggest) {
+                ideal_prob - succ_prob[suggest]
+            }
+        )
 
         # update
         mab_tracker <- mabUpdateContainer(
@@ -93,12 +117,17 @@ banditSimulation <- function(
             arm_index = mab_suggest,
             is_success = mab_success
         )
-        pgts_tracker <- pgtsUpdateContainer(
-            pgts_container = pgts_tracker,
-            arm_index = pgts_suggest,
-            instance_features = instance,
-            arms = arms,
-            is_success = pgts_success
+        pgts_trackers <- lapply(
+            X = 1:n_pgts_settings,
+            FUN = function(j) {
+                pgtsUpdateContainer(
+                    pgts_container = pgts_trackers[[j]],
+                    arm_index = pgts_suggests[[j]],
+                    instance_features = instance,
+                    arms = arms,
+                    is_success = pgts_successes[[j]]
+                )
+            }
         )
     }
 
